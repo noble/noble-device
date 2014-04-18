61bc2a7d009d88f750ee6936aba0a52cfe613ff5
Sandeep Mistry <sandeep.mistry@gmail.com>
2014-04-18 11:33:56 -0400

    - add device information service

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var NobleDevice = require('./lib/noble-device');
 
 NobleDevice.Util = require('./lib/util');
+NobleDevice.DeviceInformationService = require('./lib/device-information-service');
 NobleDevice.BatteryService = require('./lib/battery-service');
 
 module.exports = NobleDevice;
