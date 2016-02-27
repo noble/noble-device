daa8f268327c768ccc6c379712411909f34e5a42
Yuichi Tateno <hotchpotch@gmail.com>
2016-02-27 17:12:48 +0900

    Add heart rate measument service

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ var NobleDevice = require('./lib/noble-device');
 NobleDevice.Util = require('./lib/util');
 NobleDevice.DeviceInformationService = require('./lib/device-information-service');
 NobleDevice.BatteryService = require('./lib/battery-service');
+NobleDevice.HeartRateMeasumentService = require('./lib/heart-rate-measument-service.js');
 
 module.exports = NobleDevice;
---
+++ test-hrm.js
@@ -0,0 +1,45 @@
+
+var async = require('async');
+var NobleDevice = require('./index');
+
+var idOrName = process.argv[2];
+
+if (!idOrName) {
+  console.log("node ./test-hrm.js [ID or localname]")
+  process.exit(1);
+}
+
+var HRMDevice = function(device) {
+  NobleDevice.call(this, device);
+  NobleDevice.HeartRateMeasumentService.call(this);
+};
+
+HRMDevice.is = function(device) {
+  var localName = device.advertisement.localName;
+  return (device.id === idOrName || localName === idOrName);
+};
+
+NobleDevice.Util.inherits(HRMDevice, NobleDevice);
+NobleDevice.Util.mixin(HRMDevice, NobleDevice.DeviceInformationService);
+NobleDevice.Util.mixin(HRMDevice, NobleDevice.HeartRateMeasumentService);
+
+HRMDevice.discover(function(device) {
+  console.log('discovered: ' + device);
+
+  device.on('disconnect', function() {
+    console.log('disconnected!');
+    process.exit(0);
+  });
+
+  device.on('measumentChange', function(data) {
+    console.log("update measument: " + data);
+  });
+
+  device.connectAndSetUp(function(callback) {
+    console.log('connectAndSetUp');
+    device.notifyMeasument(function(counter) {
+      console.log('notifyMeasument');
+    });
+  });
+});
+
