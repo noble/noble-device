f51d8d54f8ad802a428b93871d0970462b841daf
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-08 06:52:24 +0800

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-var NobleDevice = require('./lib/noble-device');
-
-NobleDevice.Util = require('./lib/util');
-NobleDevice.DeviceInformationService = require('./lib/device-information-service');
-NobleDevice.BatteryService = require('./lib/battery-service');
-NobleDevice.HeartRateMeasumentService = require('./lib/heart-rate-measument-service.js');
-
-module.exports = NobleDevice;
