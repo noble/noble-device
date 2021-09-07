d535282103b4ad2b56415816fa930e2d8ad4cda1
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-08 06:49:30 +0800

    Delete test directory

--- test/test-index.js
+++
@@ -1,8 +0,0 @@
-var should = require('should');
-
-describe('index', function(){
-  it('should export module Util.inherits', function(){
-    var index = require('../index');
-    (index.Util.inherits).should.be.ok;
-  });
-});
